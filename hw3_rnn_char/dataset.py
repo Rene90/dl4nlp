c76751a9d7762b6dda6e344b847283acf14f6240
Rene Knaebel <reneknaebel90@gmail.com>
2016-05-02 22:04:04 +0200

    datatset should work now, still working on rnn errors

--- hw3_rnn_char/dataset.py
+++ hw3_rnn_char/dataset.py
@@ -8,37 +8,89 @@
 import numpy as np
 import pandas as pd
 
-def createH5Dataset(path,sequence):
+import h5py
+from fuel.datasets import IndexableDataset
+
+def createH5Dataset(path,corpus):
     f = h5py.File(path, mode='w')
+    sequence_length = 25
 
-    length = len(sequence)-1
+    in_splits  = corpus.get_splits(seq_len=sequence_length)
+    out_splits = corpus.get_splits(seq_len=sequence_length, shifted=True)
 
-    minibatches = 5
-    seq_len = 5
+    assert in_splits.shape == out_splits.shape
 
+    char_in  = f.create_dataset('inchar',   in_splits.shape, dtype='uint8')
+    char_out = f.create_dataset('outchar', out_splits.shape, dtype='uint8')
 
-    #char_in  = f.create_dataset('inchar', (, 117), dtype='uint8')
-    char_out = f.create_dataset('outchar', (8123, 2), dtype='uint8')
+    char_in[...] = in_splits
+    char_out[...] = out_splits
 
-    features[...] = X
-    targets[...] = y
+    f.attrs["vocab_size"] = corpus.vocab_size()
 
     f.flush()
     f.close()
 
-def tokenize_corpus(txt):
-    txt = txt.replace("\ \ ", " ")
-    txt = list(txt.lower())
-    return txt
+def createDataset(corpus=None):
+    if not corpus: corpus = Corpus(open("corpus.txt").read())
+    sequence_length = 25
+    vocab_size = corpus.vocab_size()
+    in_splits  = corpus.get_splits(seq_len=sequence_length)
+    out_splits = corpus.get_splits(seq_len=sequence_length, shifted=True)
+
+    df = IndexableDataset({
+        'inchar': in_splits.astype(np.uint8),
+        'outchar': out_splits.astype(np.uint8)
+    })
+
+    return df,vocab_size
+
+class Corpus(object):
+    def __init__(self,corpus):
+        self.encode_dict = {}
+        self.decode_dict = {}
+        self.corpus = None
+        self.__vocab_size = 0
+        self.__init(corpus)
+
+    def __init(self,corpus):
+        # prepare corpus by replacing useless whitespace
+        corpus = corpus.replace(r"  ", " ")
+        corpus = list(corpus.lower())
+        words,vocab = pd.factorize(corpus)
+        # assign internal vars
+        self.corpus = words
+        self.__vocab_size = len(vocab)
+        self.encode_dict = {idx:word for idx,word in enumerate(vocab)}
+        self.decode_dict = {word:idx for idx,word in enumerate(vocab)}
+
+    def get_splits(self,seq_len=25,shifted=False):
+        corpus = self.corpus[1:] if shifted else self.corpus[:-1]
+        off_set = len(corpus) % seq_len
+        # if last split is unequal the other ones, just skip it
+        return corpus[:-off_set].reshape((len(corpus)/seq_len,seq_len))
+
+    def vocab_size(self):
+        return self.__vocab_size
+
+    def tokenize(self):
+        return self.corpus
+
+    def encode(self,txt):
+        return [self.encode_dict.get(c, default=0) for c in txt]
+
+    def decode(self,code):
+        return [self.decode_dict.get(i, default="<unk>") for i in code]
 
 if __name__ == "__main__":
-    tokens = []
     with open("corpus.txt") as fh:
-        tokens = tokenize_corpus(fh.read())
-
-    words,vocab = pd.factorize(tokens)
-    print words, vocab
-    #createH5Dataset("dataset_rnn.hdf5", X, y)
+        corpus = Corpus(fh.read())
+    words1 = corpus.get_splits()
+    words2 = corpus.get_splits(shifted=True)
+    print words1.shape
+    print words1[:3]
+    print words2[:3]
+    createH5Dataset("dataset_rnn.hdf5", corpus)
 
 #train_data = H5PYDataset('dataset.hdf5', which_sets=('train',), load_in_memory=True)
 #test_data  = H5PYDataset('dataset.hdf5', which_sets=('test',), load_in_memory=True)
--- hw3_rnn_char/run_rnn.py
+++ hw3_rnn_char/run_rnn.py
@@ -9,41 +9,64 @@ import numpy
 import theano
 from theano import tensor
 from blocks import initialization
-from blocks.bricks import Identity
 from blocks.bricks.recurrent import SimpleRecurrent
-from blocks.bricks import Linear
+from blocks.bricks import Linear, Softmax, Tanh
 from blocks.bricks.lookup import LookupTable
 
+from dataset import createDataset
+train_data,vocab_size = createDataset()
 
+def initLayers(layers):
+    for l in layers: l.initialize()
+
+HIDDEN_DIM = 100
+VOCAB_DIM = vocab_size
 
 # input
-x = tensor.tensor3('inchar')
-y = tensor.tensor('outchar')
+x = tensor.ivector('inchar')
+y = tensor.imatrix('outchar')
 
-# recurrent history weight
-H = SimpleRecurrent(
-    dim=3,
-    activation=Tanh(),
-    weights_init=initialization.IsotropicGaussian(0.01)
-)
 #
 W = LookupTable(
-    dim = 3,
-    length=VOCAB_DIM
+    dim = HIDDEN_DIM,
+    length = VOCAB_DIM,
+    weights_init = initialization.IsotropicGaussian(0.01),
+    biases_init = initialization.Constant(0)
+)
+# recurrent history weight
+H = SimpleRecurrent(
+    dim = HIDDEN_DIM,
+    activation = Tanh(),
+    weights_init = initialization.IsotropicGaussian(0.01)
+)
+#
+S = Linear(
+    input_dim = HIDDEN_DIM,
+    output_dim = VOCAB_DIM,
     weights_init = initialization.IsotropicGaussian(0.01),
     biases_init = initialization.Constant(0)
 )
 
-S = Linear(
-    input_dim = HIDDEN_DIM,
-    output_dim = VOCAB_DIM
+A = Softmax()
+
+initLayers([W,H,S])
+y_hat = A.apply(S.apply(H.apply(W.apply(x))))
+cost = softmax.categorical_cross_entropy(y, y_hat).mean()
+
+main_loop = MainLoop(
+    data_stream = DataStream(
+        train_data
+    ),
+    algorithm   = GradientDescent(
+        cost=cost,
+        parameters=cg.parameters,
+        step_rule=Scale(learning_rate=0.1)),
+        extensions = [
+            DataStreamMonitoring(variables=[cost]),
+            FinishAfter(after_n_epochs=10),
+            Printing(),
+            #TrainingDataMonitoring([cost,], after_batch=True),
+        ]
 )
-
-H.initialize()
-W.initialize()
-
-out = H.apply(W.apply(x))
-
-cost = softmax.categorical_cross_entropy(y, y_hat, extra_ndim=1).mean()
-
-f = theano.function([x], out, updates=...)
+main_loop.run()
+#f = theano.function([x], out, updates=...)
